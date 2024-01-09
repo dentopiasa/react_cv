3f003a53fc3b5acec98e7f8f7e09d1905fa4c20c
dentopiasa <castelli_philippe@hotmail.com>
2024-01-09 20:46:45 +0200

    ff

--- cv/src/components/Exercise.jsx
+++ cv/src/components/Exercise.jsx
@@ -12,11 +12,11 @@ function Profile() {
   export default function Gallery() {
     return (
       <section>
-        <h1>Amazing scientistsff</h1>
+        <h1>Amazing scientists</h1>
         <Profile />
         <Profile />
         <Profile />
       </section>
     );
   }
-    
+  
